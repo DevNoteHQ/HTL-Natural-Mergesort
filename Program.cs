fe099bea6edbfde29c3ee62daf136e848c147c26
DevNoteHQ <schoepf.mathias@gmail.com>
2018-01-11 00:13:59 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HTL_Natural_Mergesort
+{
+    class Program
+    {
+        const int iElements = 10;
+        const int iMax = 10000;
+        static void Main(string[] args)
+        {
+            Sort<int> Sorting = new Sort<int>();
+
+            int[] List = new int[iElements];
+            CreateRandomIntArray(ref List, iMax);
+
+
+
+            Console.ReadLine();
+        }
+
+        public static void CreateRandomIntArray(ref int[] List, int iMax)
+        {
+            Random random = new Random();
+
+            for(int i = 0; i < List.Length; i++)
+            {
+                List[i] = random.Next(iMax);
+            }
+        }
+    }
+}
---
+++ Sort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTL_Natural_Mergesort
+{
+    class Sort
+    {
+        public Sort()
+        {
+
+        }
+
+        public void MergeSort(int[] List)
+        {
+            List<List<int>> Elements = new List<List<int>>();
+            for(int i = 0; i < List.Length - 1; i++)
+            {
+                List<int> Element = new List<int>();
+                do
+                {
+                    Element.Add(List[i]);
+                } while (List[i] <= List[i + 1]);
+                Elements.Add(Element);
+            }
+        }
+    }
+
+    abstract class SortTools<T>
+    {
+        public static void Swap(ref T A, ref T B)
+        {
+            T Buffer = A; A = B; B = Buffer;
+        }
+    }
+}
