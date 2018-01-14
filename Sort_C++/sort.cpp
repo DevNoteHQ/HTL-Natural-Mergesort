19b2528d11a7abc107264bf890c9a3f5355ad3c6
DevNoteHQ <schoepf.mathias@gmail.com>
2018-01-14 17:32:40 +0100

    C++-Projekt added

---
+++ Sort_C++/sort.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "sort.h"
+
+namespace Sort
+{
+
+}
---
+++ Sort_C++/sort.h
@@ -0,0 +1,14 @@
+
+#ifndef SORT_H
+#define SORT_H
+
+namespace Sort
+{
+	void Sort(int* List, int iElements);
+	void Merge(int* A, int* B, int* Target);
+}
+
+#endif
+
+
+
